f63b1f1d38c40e426fd84d7a66362107eec61aeb
filip pacurar <filip@pacurar.net>
2015-07-23 13:43:39 +0300

    add export link and match new model from daftspunk's commit

--- models/EntriesExport.php
+++ models/EntriesExport.php
@@ -1,5 +1,11 @@
 <?php
 namespace Acme\Importexport\Models;
 class EntriesExport extends \Backend\Models\ExportModel {
+    public function exportData($columns, $sessionKey = null) {
 
+    }
+
+    public function getCityOptions($keyValue = null) {
+        return ['New-York' => 'New-York','Los Angeles'=>'Los Angeles'];
+    }
 }
