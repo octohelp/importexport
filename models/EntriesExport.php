01a1f39e845511f46978eb566559c0b66349d4ee
Filip Pacurar <Admin@192-168-0-101.rdsnet.ro>
2015-07-27 21:24:46 +0300

    final

--- models/EntriesExport.php
+++ models/EntriesExport.php
@@ -3,8 +3,11 @@ namespace Octohelp\Importexport\Models;
 
 class EntriesExport extends \Backend\Models\ExportModel {
     public function exportData($columns, $sessionKey = null) {
-        dd(Entries::get()->addVisible($columns)->toArray());
-        return Entries::addVisible($columns)->get()->toArray();
+        $entries = Entries::all();
+        $entries->each(function($subscriber) use ($columns) {
+            $subscriber->addVisible($columns);
+        });
+        return $entries->toArray();
     }
 
     public function getCityOptions($keyValue = null) {
