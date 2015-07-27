484778390268a572243dbd2e7e8c05a00cbf83f6
filip pacurar <filip@pacurar.net>
2015-07-27 20:28:59 +0300

    make it work

--- models/EntriesExport.php
+++ models/EntriesExport.php
@@ -1,8 +1,10 @@
 <?php
 namespace Acme\Importexport\Models;
+
 class EntriesExport extends \Backend\Models\ExportModel {
     public function exportData($columns, $sessionKey = null) {
-
+        dd(Entries::get()->addVisible($columns)->toArray());
+        return Entries::addVisible($columns)->get()->toArray();
     }
 
     public function getCityOptions($keyValue = null) {
